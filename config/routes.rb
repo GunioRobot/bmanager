f6cc63080a7b5254e34081d8fe58d10c40342b40
neverett <n.everett@gmail.com>
2011-09-21 11:28:04 -0700

    added static pages and CSS, finished routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
-Bmanager::Application.routes.draw do
-  get "pages/home"
+Bmanager::Application.routes.draw do 
+  get "users/new"
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
 
-  get "pages/contact"
-
-  get "pages/about"
-
-  get "pages/help"
+  root :to => 'pages#home'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -53,11 +53,7 @@ Bmanager::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => "welcome#index"
-
+ 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
